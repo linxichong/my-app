05726f43a3d9e2c7b26be624ef4693cbe57a6c6f
linxichong <gogailinxichong@gmail.com>
2019-07-17 18:31:45 +0900

    macro尝试
    Signed-off-by: linxichong <gogailinxichong@gmail.com>

--- src/containers/About.tsx
+++ src/containers/About.tsx
@@ -1,7 +1,35 @@
 import * as React from "react";
+import { state, bind } from "reactive.macro";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
 
 const About: React.SFC = () => {
-  return <div>About</div>;
+  const a = state(1);
+  const b = state(2);
+
+  return (
+    <Title>
+      <Wrapper>About</Wrapper>
+      <div>
+        <input type="number" value={bind(a)} />
+        <button onClick={b => b += 1}>b+</button>
+
+        <p>
+          {a} + {b} = {a + b}
+        </p>
+      </div>
+    </Title>
+  );
 };
 
 export default About;
--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -10,7 +10,6 @@ import ErrorBoundary from "./ErrorBoundary";
 
 const About = React.lazy(() => import("./About"));
 const NovelContainer = React.lazy(() => import("./NovelContainer"));
-const MaterialUI = React.lazy(() => import("./MaterialUI"));
 
 class App extends React.Component {
   render() {
@@ -23,7 +22,6 @@ class App extends React.Component {
             <React.Suspense fallback={<div>Loading...</div>}>
               <Route exact path={PATHS.ABOUT} component={About} />
               <Route exact path={PATHS.NOVELS} component={NovelContainer} />
-              <Route exact path={PATHS.UI} component={MaterialUI} />
             </React.Suspense>
           </ErrorBoundary>
         </ConnectedRouter>
